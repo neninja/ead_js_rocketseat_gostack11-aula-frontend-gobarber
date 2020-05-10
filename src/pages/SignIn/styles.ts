0a2b615da68147cd624324adf7c2cf5d5cf2c44a
Felipe Silva <felipe.dacs@gmail.com>
2020-05-10 03:49:40 -0300

    release: add aula 21

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { shade } from 'polished';
 
 import signInBackgroundImg from '../../assets/sign-in-background.png';
@@ -13,12 +13,31 @@ export const Container = styled.div`
 export const Content = styled.div`
   display: flex;
   flex-direction: column;
-
   align-items: center;
-  place-content: center;
+  justify-content:center;
 
   width: 100%;
   max-width 700px;
+`;
+
+const appearFromLeft = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(-50px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
+
+export const AnimationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  animation: ${appearFromLeft} 1s;
 
   form {
     margin: 80px 0;
@@ -37,7 +56,7 @@ export const Content = styled.div`
       transition: color 0.2s;
 
       &:hover {
-        color: ${shade(0.2, '#f4ede8')}
+        color: ${shade(0.2, '#f4ede8')};
       }
     }
   }
@@ -45,21 +64,21 @@ export const Content = styled.div`
   // estiliza <a> um nivel após o componente, e não todo <a>
   // isso evita que estilize "esqueci minha senha"
   > a {
-      color: #ff9000;
-      display: block;
-      margin-top: 24px;
-      text-decoration: none;
-      transition: color 0.2s;
+    color: #ff9000;
+    display: block;
+    margin-top: 24px;
+    text-decoration: none;
+    transition: color 0.2s;
 
-      display: flex;
-      align-items: center;
+    display: flex;
+    align-items: center;
 
-      svg {
-        margin-right: 16px;
-      }
-      &:hover {
-        color: ${shade(0.2, '#ff9000')}
-      }
+    svg {
+      margin-right: 16px;
+    }
+    &:hover {
+      color: ${shade(0.2, '#ff9000')};
+    }
   }
 `;
 

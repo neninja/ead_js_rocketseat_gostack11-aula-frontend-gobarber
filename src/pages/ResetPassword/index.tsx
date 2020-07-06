80afb8911c479819194e060147547881b0885f8d
Felipe Silva <felipe.dacs@gmail.com>
2020-07-06 00:13:32 -0300

    :tv: add aula 26

--- src/pages/ResetPassword/index.tsx
+++ src/pages/ResetPassword/index.tsx
@@ -3,7 +3,7 @@ import { FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 
 import { useToast } from '../../hooks/toast';
 import getValidationErrors from '../../utils/getValidationErrors';
@@ -14,6 +14,7 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import { Container, Content, AnimationContainer, Background } from './styles';
+import api from '../../services/api';
 
 interface ResetPasswordFormData {
   password: string;
@@ -23,7 +24,9 @@ interface ResetPasswordFormData {
 const ResetPassword: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
   const { addToast } = useToast();
+
   const history = useHistory();
+  const location = useLocation();
 
   const handleSubit = useCallback(
     async (data: ResetPasswordFormData) => {
@@ -43,9 +46,22 @@ const ResetPassword: React.FC = () => {
           abortEarly: false,
         });
 
+        const { password, password_confirmation } = data;
+        const token = location.search.replace('?token=', '');
+
+        if (!token) {
+          throw new Error();
+        }
+
+        api.post('/password/reset', {
+          password,
+          password_confirmation,
+          token,
+        });
+
         // se não caiu no catch significa que foi sucesso
 
-        history.push('/signin');
+        history.push('/');
       } catch (err) {
         if (err instanceof Yup.ValidationError) {
           const errors = getValidationErrors(err);
@@ -62,7 +78,7 @@ const ResetPassword: React.FC = () => {
       }
     },
     // toda variável externa deve ser uma dependência do useCallback
-    [addToast, history],
+    [addToast, history, location.search],
   );
 
   return (

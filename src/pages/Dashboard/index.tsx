808520268df2996d29994ecc33a01ddfb08082ed
Felipe Silva <felipe.dacs@gmail.com>
2020-07-07 22:13:38 -0300

    :tv: add aula 34

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useCallback, useEffect, useMemo } from 'react';
+import { Link } from 'react-router-dom';
 import { isToday, format, parseISO, isAfter } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import DayPicker, { DayModifiers } from 'react-day-picker';
@@ -146,7 +147,9 @@ const Dashboard: React.FC = () => {
           </Profile>
           <div>
             <span>Bem-vindo,</span>
-            <strong>{user.name}</strong>
+            <Link to="/profile">
+              <strong>{user.name}</strong>
+            </Link>
           </div>
           <button type="button" onClick={signOut}>
             <FiPower />
